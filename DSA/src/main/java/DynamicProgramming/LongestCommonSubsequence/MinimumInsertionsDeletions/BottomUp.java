4975e79a8a930ca7720b06b04f34deb8ecc9c5d4
Sushant <email.sushant.patil@gmail.com>
2026-03-28 20:05:34 +0530

    Add code for minimum number of insertions and deletions problem

---
+++ DSA/src/main/java/DynamicProgramming/LongestCommonSubsequence/MinimumInsertionsDeletions/BottomUp.java
@@ -0,0 +1,38 @@
+package DynamicProgramming.LongestCommonSubsequence.MinimumInsertionsDeletions;
+
+public class BottomUp {
+    public static void main(String[] args) {
+        String s1 = "abcdgh";
+        String s2 = "abedfhr";
+        int m = s1.length();
+        int n = s2.length();
+
+        System.out.println("=============== " + minimumInsertionsDeletions(s1, s2, m, n) + " ===============");
+    }
+
+    public static int minimumInsertionsDeletions(String s1, String s2, int m, int n) {
+        // create and initialize dp
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        // fill up the table
+        // logic similar to LCS
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return m + n - 2 * dp[m][n];
+    }
+}
---
+++ DSA/src/main/java/DynamicProgramming/LongestCommonSubsequence/MinimumInsertionsDeletions/statement.md
@@ -0,0 +1,7 @@
+Given two strings, return minimum number of insertions and deletions required to convert one string into another.
+
+E.g. s1 = “abcdgh”
+
+s2 = “abedfhr”
+
+Ans → 5 → delete c and g - add e, f and r
