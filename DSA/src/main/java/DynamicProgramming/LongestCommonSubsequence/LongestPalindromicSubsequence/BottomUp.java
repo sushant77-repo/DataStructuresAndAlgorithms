83dd0c0fd881e42c995e719e35e58080f18d56c5
Sushant <email.sushant.patil@gmail.com>
2026-03-31 14:29:52 +0530

    Add code for longest palindromic subsequence problem

---
+++ DSA/src/main/java/DynamicProgramming/LongestCommonSubsequence/LongestPalindromicSubsequence/BottomUp.java
@@ -0,0 +1,42 @@
+package DynamicProgramming.LongestCommonSubsequence.LongestPalindromicSubsequence;
+
+public class BottomUp {
+    public static void main(String[] args) {
+        String s1 = "agbcba";
+
+        System.out.println("=============== " + longestPalindromicSubsequence(s1) + " ===============");
+    }
+
+    public static int longestPalindromicSubsequence(String s) {
+        // reverse the given string
+        String rev = new StringBuilder(s).reverse().toString();
+
+        int m = s.length();
+        int n = rev.length();
+
+        // create and initialize dp
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        // fill up the table
+        // logic similar to LCS
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == rev.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+
+        return dp[m][n];
+    }
+}
---
+++ DSA/src/main/java/DynamicProgramming/LongestCommonSubsequence/LongestPalindromicSubsequence/statement.md
@@ -0,0 +1,9 @@
+**Leetcode 516 - Longest Palindromic SubSequence**
+
+https://leetcode.com/problems/longest-palindromic-subsequence/description/
+
+Given a string, return the length of longest palindromic subsequence.
+
+E.g. s = “agbcba”
+
+Ans → 5 → Longest palindromic subsequence is “abcba”
